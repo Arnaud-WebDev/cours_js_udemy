dc850e9126020ed65c4e5a2d7bdab7d6417bbe5b
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-11 15:04:38 +0100

    Ajout de l'exercice 26 et 27

--- js/cours_js.js
+++ js/cours_js.js
@@ -12,4 +12,16 @@ const r1 = Math.min(a, b, c);
 const r3 = Math.max(a, b, c);
 const r2 = a + b + c - r1 - r3;
 
-console.log(r2);
+//---------------------------
+//        Exercice 27
+//---------------------------
+
+let i = 50;
+
+while (i > 10) {
+  i--;
+}
+
+const exercice_reussi = true;
+
+console.log(i);
