1d5796a845fa4e5894879e6029333c4fc4d0a2bb
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-12 20:33:54 +0100

    Ajout de l'exercice 29

--- js/cours_js.js
+++ js/cours_js.js
@@ -32,7 +32,47 @@ const exercice_reussi = true;
 
 function showIndividualLetters(mot) {
   for (let i = 0; i < mot.length; i++) {
-    console.log(mot[i]);
+    //console.log(mot[i]);
   }
 }
 showIndividualLetters('JavaScript');
+
+//---------------------------
+//        Exercice 29
+//---------------------------
+
+/* function compareStrings(s1, s2) {
+  if (s1 === s2) {
+    return true;
+  } else {
+    return false;
+  }
+}                                                     // Solution trouvé par moi même !
+
+// Ne modifiez pas les lignes ci-dessous
+let firstCheck = compareStrings('Hello world', 'Hello world');
+let secondCheck = compareStrings('Hello world', 'Goodbye world');
+
+console.log(firstCheck);
+console.log(secondCheck);
+ */
+
+function compareStrings(s1, s2) {
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  for (let i = 0; i < s1.length; i++) {
+    let char1 = s1[i];
+    let char2 = s2[i];
+    if (char1 !== char2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Ne modifiez pas les lignes ci-dessous
+let firstCheck = compareStrings('Hello world', 'Hello world');
+let secondCheck = compareStrings('Hello world', 'Goodbye world');
+console.log(firstCheck);
+console.log(secondCheck);
