bbc1905d279519c0f63ff2825db7320d2210ba2f
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-13 15:41:11 +0100

    Exercice 31 fini

--- js/cours_js_30-35.js
+++ js/cours_js_30-35.js
@@ -1,15 +1,31 @@
-function sumNumber(number) {
+//---------------------------
+//        Exercice 31
+//---------------------------function sumNumber(number) {
   let result = number.toString().split('');
-  result.forEach((e) => console.log(e));
+  let result2 = result.map(Number);
+  console.log(result2);
 
-  /* let reducer = (previousValue, currentVallue) => previousValue + currentVallue;
-  console.log(result.reduce(reducer)); */
+  let reducer = (previousValue, currentValue) => previousValue + currentValue;
+  return result2.reduce(reducer);
 }
 
 // Ne modifiez pas la ligne ci-dessous
 let sum = sumNumber(31245);
+console.log(sum);
 
-/* for (i = 0; i < result.length; i++) {
-    console.log([i]);
+function sumNumber(number) {
+  let digitsAsList = number.toString().split('');
+  let digits = digitsAsList.map(Number);
+  let sum = 0;
+  for (let digit of digits) {
+    sum += digit;
   }
-  console.log(result); */
+  return sum;
+}
+
+let sum = sumNumber(31245);
+
+
+
+
+
