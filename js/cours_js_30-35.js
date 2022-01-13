183c130706a0bae04549fb8b4dae0bd7c1fa62b2
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-13 18:37:22 +0100

    Fin de l'Exercice 32

--- js/cours_js_30-35.js
+++ js/cours_js_30-35.js
@@ -29,3 +29,32 @@ let sum = sumNumber(31245);
 //---------------------------
 //        Exercice 32
 //---------------------------
+
+// Ne modifiez pas la ligne ci-dessous
+let names = ['Pierre', 'Marie', 'Julie', 'Adrien', 'Julie'];
+
+// Écrivez votre code ci-dessous
+
+let names_replaced;
+names_replaced = names.map((x) => x.replace('Julie', 'Julien'));
+
+//console.log(names_replaced);
+
+//Solution alternative sans map()
+
+/* let names_replaced = [];
+for (let name of names) {
+    names_replaced.push(name.replace('Julie', 'Julien'));
+}
+console.log(names_replaced); */
+
+//Solution alternative sans map()
+
+let names = ['Pierre', 'Marie', 'Julie', 'Adrien', 'Julie'];
+
+let names_replaced = [];
+
+for (let name of names) {
+  names_replaced.push(name.replace('Julie', 'Julien'));
+}
+console.log(names_replaced);
