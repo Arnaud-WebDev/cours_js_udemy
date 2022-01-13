11e5100b715c576f280e9974ba1824d83a230614
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-01-13 12:35:11 +0100

    Ajout de l'exercice 31 + photo et changement du menu déroulant en responsive

---
+++ js/cours_js_30-35.js
@@ -0,0 +1,14 @@
+function sumNumber(number) {
+  let result = Math.abs(number).toString().split('');
+  result.forEach((e) => console.log(e));
+  /* let reducer = (previousValue, currentVallue) => previousValue + currentVallue;
+  console.log(result.reduce(reducer)); */
+}
+
+// Ne modifiez pas la ligne ci-dessous
+let sum = sumNumber(31245);
+
+/* for (i = 0; i < result.length; i++) {
+    console.log([i]);
+  }
+  console.log(result); */
